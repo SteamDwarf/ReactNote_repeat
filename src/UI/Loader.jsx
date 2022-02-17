d409c2f93bea223fda5df305b5aa29cb9a933319
SteamDwarf <schajckodmitrij@gmail.com>
2022-02-17 11:39:07 +0300

    added theme style for loader

--- src/UI/Loader.jsx
+++ src/UI/Loader.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import classes from './Loader.module.css'
+import classes from './Loader.module.css';
+import '../styles/themes/components/loader.scss';
 
 function Loader({text}) {
     return (
         <div className={classes.loadingBlock}>
             <h1 className='title'>{text}</h1>
-            <div className={classes.spinner}></div>
+            <div className={`${classes.spinner} spinner light`}></div>
         </div>
     )
 }
